12d5b8317429bc896c873b2fb0e4bd99ca681564
ghOst-vedant <gaikwadvedant30@gmail.com>
2024-06-25 14:21:07 +0530

    user register Router

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,18 @@
 import express from 'express'
 
+import errorHandler from './middleware/errorhandler'
+import userRouter from './routes/userRouter'
+
 const app=express()
 
+// methods
+app.use("/api/users",userRouter)
+app.get(`/`,(req,res)=>{
 
-app.get(`/`,(req,res,next)=>{
     res.json({message:"Learning RESTApi."})
 })
 
+// error handler
+
+app.use(errorHandler)
 export default app
--- src/config/config.ts
+++ src/config/config.ts
@@ -2,8 +2,9 @@ import {config as configuration} from 'dotenv'
 
 configuration()
 const _config={
-    port:process.env.PORT,
-    dbUrl:process.env.MONGO
+    port:process.env.PORT!,
+    dbUrl:process.env.MONGO!,
+    env:process.env.NODE_ENV!
 }
 
 export const config = Object.freeze(_config)
