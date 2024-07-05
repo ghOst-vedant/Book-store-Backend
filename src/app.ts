21f110eca2daa25913389f3ce6023a7b2fd58b20
ghOst-vedant <gaikwadvedant30@gmail.com>
2024-07-05 12:39:21 +0530

    enabled cors

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,16 @@ import express, { Request, Response } from "express"
 import errorHandler from "./middleware/errorhandler"
 import userRouter from "./routes/userRouter"
 import bookRouter from "./routes/bookRouter"
-
+import cors from "cors"
+import { config } from "./config/config"
 const app = express()
 
 app.use(express.json())
+app.use(
+    cors({
+        origin: config.domain,
+    }),
+)
 // methods
 app.use("/api/users", userRouter)
 app.use("/api/books", bookRouter)
--- src/config/config.ts
+++ src/config/config.ts
@@ -9,6 +9,7 @@ const _config = {
     cloud_name: process.env.CLOUDINARY_NAME!,
     cloud_api: process.env.CLOUDINARY_API_KEY!,
     cloud_secret: process.env.CLOUDINARY_SECRET!,
+    domain: process.env.origin!,
 }
 
 export const config = Object.freeze(_config)
